90fd08f847a5d464be76b58c815f794e4de4e808
Kevin Smith <ksmithbaylor@gmail.com>
2015-09-16 20:49:30 -0400

    Latest changes

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,13 @@
 class Tweet < ActiveRecord::Base
   belongs_to :user
+
+  validates :message, {
+    presence: true,
+    length: {
+      maximum: 140,
+      minimum: 10,
+      too_long: "A tweet is only 140 max. Everybody knows that!",
+      too_short: "Write more!"
+    }
+  }
 end
